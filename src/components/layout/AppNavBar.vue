0e5a96708c8210537f270a0b7ec0ef05a0b38591
victorhugoganzenmuller <victorhugog2910@gmail.com>
2026-09-25 16:44:23 -0300

    Mudança 25/09

--- src/components/layout/AppNavBar.vue
+++ src/components/layout/AppNavBar.vue
@@ -1,13 +1,53 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
 <template>
-    <!-- As a link -->
-<nav class="navbar bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">petSchool</a>
-  </div>
+  <nav
+    class="navbar navbar-expand-md navbar-dark bg-success"
+    aria-label="Navegação principal"
+  >
+    <div class="container-fluid">
+      <RouterLink
+        class="navbar-brand fw-semibold"
+        to="/pets"
+      >
+        <i
+          class="bi bi-heart-fill me-2"
+          aria-hidden="true"
+        ></i>
+        PetSchool
+      </RouterLink>
 
-  <div>
-    <routerlink></routerlink>
-  </div>
-</nav>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#petschool-navbar"
+        aria-controls="petschool-navbar"
+        aria-expanded="false"
+        aria-label="Abrir menu principal"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
+      <div
+        id="petschool-navbar"
+        class="collapse navbar-collapse"
+      >
+        <div class="navbar-nav ms-auto">
+          <RouterLink
+            class="nav-link"
+            to="/pets"
+            >Pets</RouterLink
+          >
+          <RouterLink
+            class="nav-link"
+            to="/pets/novo"
+            >Novo pet</RouterLink
+          >
+        </div>
+      </div>
+    </div>
+  </nav>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -5,9 +5,18 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: () => import('../views/HomeView.vue'),
+      redirect: '/pets',
     },
+    {
+    path: '/pets',
+    name: 'pets',
+    component: () => import('../views/PetsView.vue')
+    },
+    {
+    path: '/pets/novo',
+    name: 'petsNovo',
+    component: () => import('../views/AddPetsView.vue')
+    }
   ],
 });
 
